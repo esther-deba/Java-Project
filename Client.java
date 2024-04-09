9cdc85dae87dbb8b392ba352b684277b97fa3781
Yanisdel <yanisdelleci03@gmail.com>
2024-04-09 15:20:53 +0200

    constructeurs

--- Client.java
+++ Client.java
@@ -6,17 +6,16 @@ import java.util.*;
 public class Client extends Personne {
 
    
-    public Client() {
+   
+    public Client(String nom,String prenom, String num ,String adresse ,Magasin magasin) {
+     super(nom,prenom,num,adresse);
+     this.magasin =magasin;
     }
 
-    /**
-     * 
-     */
+    
     public Magasin magasin;
 
-    /**
-     * 
-     */
+    
     public Vector<Commande> listCommandes = new Vector<Commande>();
 
 }
--- Personne.java
+++ Personne.java
@@ -2,35 +2,29 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
+
+
 public class Personne {
 
-    /**
-     * Default constructor
-     */
-    public Personne() {
-    }
+   
+    public Personne(String nom ,String prenom,String num,String adresse){
+       this.nom = nom;
+       this.prenom = prenom;
+       this.num = num;
+       this.adresse =adresse;
+    } 
+    
 
-    /**
-     * 
-     */
+
     public String nom;
 
-    /**
-     * 
-     */
+    
     public String prenom;
 
-    /**
-     * 
-     */
+    
     public String num;
 
-    /**
-     * 
-     */
+    
     public String adresse;
 
 }
