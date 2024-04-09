f688bee170e7d1c51a9e79f6baf045d44cda534b
Yanisdel <yanisdelleci03@gmail.com>
2024-04-09 15:51:28 +0200

    constructeurs

--- Depot.java
+++ Depot.java
@@ -2,32 +2,20 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
 public class Depot {
 
-    /**
-     * Default constructor
-     */
-    public Depot() {
+    public Depot(Magasin magasin, String adresse ) {
+        this.magasin = magasin;
+        this.adresse = adresse ;
     }
 
-    /**
-     * 
-     */
+   
     public String adresse;
 
-    /**
-     * 
-     */
     public Magasin magasin;
 
 
 
-    /**
-     * 
-     */
     public Vector<LigneStock> listStock = new Vector<LigneStock>();
 
 }
