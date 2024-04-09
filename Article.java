2a2327eca1ae8ff3ec70e86d4e925d35546f9ea8
esther-deba <debaesther72@gmail.com>
2024-04-09 14:06:36 +0200

    try

--- Article.java
+++ Article.java
@@ -2,45 +2,30 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
+
 public class Article {
 
-    /**
-     * Default constructor
-     */
+   
     public Article() {
     }
 
-    /**
-     * 
-     */
+   
     public String nom;
 
-    /**
-     * 
-     */
+   
     public String taille;
 
-    /**
-     * 
-     */
+    
     public String couleur;
 
-    /**
-     * 
-     */
+  
+   
     public float prix;
 
-    /**
-     * 
-     */
+   
     public Vector<LigneStock> listStock = new Vector<LigneStock>();
 
-    /**
-     * 
-     */
+   
     public Vector<LigneCommande> listLigneCmd = new Vector<LigneCommande>();
 
 }
