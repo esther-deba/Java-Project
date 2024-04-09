bb80378d2ba1017fc86ed399c7ff659de5a49f1f
esther-deba <debaesther72@gmail.com>
2024-04-09 16:01:09 +0200

    construteurs

--- Commande.java
+++ Commande.java
@@ -1,36 +1,14 @@
-
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
 public class Commande {
 
-    /**
-     * Default constructor
-     */
-    public Commande() {
+    public Commande(Date date, Vendeur vendeur, Client client) {
+        this.date = date;
+        this.vendeur = vendeur;
+        this.client = client;
     }
-
-    /**
-     * 
-     */
     public Date date;
-
-    /**
-     * 
-     */
     public Vendeur vendeur;
-
-    /**
-     * 
-     */
     public Client client;
-
-    /**
-     * 
-     */
-    public Vector<LigneCommande> listLigneCmd = new Vector<LigneCommande>();
-
 }
--- LigneStock.java
+++ LigneStock.java
@@ -1,31 +1,16 @@
-
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
 public class LigneStock {
 
-    /**
-     * Default constructor
-     */
-    public LigneStock() {
+    public LigneStock(int qte, Depot depot, Article article) {
+        this.qte = qte;
+        this.depot = depot;
+        this.article = article;
     }
 
-    /**
-     * 
-     */
     public int qte;
-
-    /**
-     * 
-     */
     public Depot depot;
-
-    /**
-     * 
-     */
     public Article article;
 
 }
