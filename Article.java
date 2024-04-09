ab977aaa2cb642919a28ee7ddc5bb553e2a7dfc3
esther-deba <debaesther72@gmail.com>
2024-04-09 15:31:50 +0200

    constructeurs

--- Article.java
+++ Article.java
@@ -1,31 +1,21 @@
-
 import java.io.*;
 import java.util.*;
 
 
 public class Article {
-
-   
-    public Article() {
+    public Article(String nom, String taille, String couleur, double prix) {
+        this.nom = nom;
+        this.taille = taille;
+        this.couleur = couleur;
+        this.prix = prix;
     }
-
-   
     public String nom;
-
-   
     public String taille;
-
-    
     public String couleur;
-
-  
-   
-    public float prix;
-
-   
+    public double prix;
+    
     public Vector<LigneStock> listStock = new Vector<LigneStock>();
-
-   
     public Vector<LigneCommande> listLigneCmd = new Vector<LigneCommande>();
+
 
 }
--- LigneCommande.java
+++ LigneCommande.java
@@ -1,31 +1,17 @@
-
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
+
 public class LigneCommande {
 
-    /**
-     * Default constructor
-     */
-    public LigneCommande() {
+    public LigneCommande(int qte, Commande commande, Article article) {
+        this.qte = qte;
+        this.commande = commande;
+        this.article = article;
     }
 
-    /**
-     * 
-     */
     public int qte;
-
-    /**
-     * 
-     */
     public Commande commande;
-
-    /**
-     * 
-     */
     public Article article;
 
 }
