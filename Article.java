10d8defed5508d473e34130f46c1d197ab176deb
esther-deba <debaesther72@gmail.com>
2024-05-05 14:26:36 +0200

    some changes

--- Article.java
+++ Article.java
@@ -3,16 +3,15 @@ import java.util.*;
 
 
 public class Article {
-    public Article(String nom, String taille, String couleur, double prix) {
+
+    public Article(String nom, double prix, String description) {
         this.nom = nom;
-        this.taille = taille;
-        this.couleur = couleur;
         this.prix = prix;
+        this.description = description;
     }
     public String nom;
-    public String taille;
-    public String couleur;
     public double prix;
+    public String description;
     
     public Vector<LigneStock> listStock = new Vector<LigneStock>();
     public Vector<LigneCommande> listLigneCmd = new Vector<LigneCommande>();
--- Depot.java
+++ Depot.java
@@ -20,18 +20,30 @@ public class Depot {
     }
 
     public void supprimerLigneStock(LigneStock ligneStock){
-        //à VERIFIER !!!!!!!!!!!!!
-        listLigneStock.removeIf(element -> (element.article.nom == ligneStock.article.nom && element.article.taille == ligneStock.article.taille && element.article.couleur == ligneStock.article.couleur));
+        listLigneStock.removeIf(element -> element.article.nom == ligneStock.article.nom);
+        //OBSERVABLE
     }
 
-    public LigneStock rechercherLigneStock(String n, String t, String coul){
+    public LigneStock rechercherLigneStock(String nom){
         for(int i=0; i<listLigneStock.size();i++){
-            if(listLigneStock.get(i).article.nom == n && listLigneStock.get(i).article.taille == t && listLigneStock.get(i).article.couleur == coul){return listLigneStock.get(i);}
+            if(listLigneStock.get(i).article.nom == nom ){
+                return listLigneStock.get(i);
+            }
         }
         return null;
     }
+
+    private LigneStock rechercherLigneStock(Article art) {
+        // recherche un article à utiliser qu'à l'interieur de la classe !
+        for( int i=0; i<listLigneStock.size();i++){
+            if( listLigneStock.get(i).article == art ){
+                return listLigneStock.get(i);
+            }
+        }
+        return null;
+    }
+
     public void modifierQuantiteStock(Article art, int q){
-        rechercherLigneStock(art.nom, art.taille, art.couleur).qte = q;
-        //des que la commande est validée la valeur de qte doit décrementer
+        rechercherLigneStock(art).qte = q;
     }
 }
