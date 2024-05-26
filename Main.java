5865a1f7a45ed7f799ef0d3ef99af95c96eee5c9
esther-deba <debaesther72@gmail.com>
2024-05-26 16:22:20 +0200

    final

--- Main.java
+++ Main.java
@@ -13,32 +13,35 @@ public class Main {
 	public static void main(String[] args) throws UnsupportedLookAndFeelException {
 		// TODO Auto-generated method stub
 		UIManager.setLookAndFeel(new NimbusLookAndFeel());
-		Magasin m = new Magasin("ITools");
-		Depot d = new Depot(m,"ad");
-		Menu menu = new Menu(m);
-	    //GestionProduit p = new GestionProduit(d);
-		//GestionClient c = new GestionClient(m);
-		Article pcPortable = new Article("Pc Portable",600,"Pc portable de marque HP i7 1To SSD Windows10");
-        Article iphone15 = new Article("iPhone 15",1400,"La nouvelle pépite Apple est enfin la !");
-        Article macBook = new Article("MacBook",900,"L'ordinateur intélligent d'apple !");
-        Article tv = new Article("TV",200,"Télévision énorme avec une dalle OLED 4K");
-        d.ajouteLigneStock(new LigneStock(30,d,pcPortable));
-        d.ajouteLigneStock(new LigneStock(45,d,iphone15));
-        d.ajouteLigneStock(new LigneStock(10,d,macBook));
-        d.ajouteLigneStock(new LigneStock(70,d,tv));
-		m.ajouteStock(d);
-		Client client = new Client("DEBA", "Esther", "0365695", "dejhif",m);
-		Vendeur vendeur = new Vendeur("DEBA", "Esther", "0365695", "dejhif",m);
-		m.ajouteClient(client);
-		m.ajouteVendeur(vendeur);
+		Magasin magasin = new Magasin("ITools");
+		Depot depot = new Depot(magasin,"03 Avenue Germaine");
+		Menu menu = new Menu(magasin);
+		Article pcDell = new Article("Pc DELL",600,"Bon état");
+  Article iphone15 = new Article("iPhone 15",1400,"Neuf");
+		Article iphone14Pro = new Article("iPhone 14 pro",900,"Reconditionné");
+  Article cleUSB = new Article("Clé USB",80,"16 GO");
+  depot.ajouteLigneStock(new LigneStock(30,depot,pcDell));
+  depot.ajouteLigneStock(new LigneStock(40,depot,iphone15));
+  depot.ajouteLigneStock(new LigneStock(5,depot,iphone14Pro));
+  depot.ajouteLigneStock(new LigneStock(100,depot,cleUSB));
+		magasin.ajouteStock(depot);
+		Client client = new Client("DEBA", "esther", "0123456789", "27 rue d'Alger",magasin);
+		Client client1 = new Client("DUPONT", "samy", "0123456789", "26 rue d'Alger",magasin);
+		Client client2 = new Client("PIERRE", "hugo", "0123456789", "10 rue barbes",magasin);
+		Vendeur vendeur = new Vendeur("DELLECI", "yanis", "0123456789", "26 rue de Telemcen",magasin);
+		Vendeur vendeur2 = new Vendeur("JEAN", "luc", "0123456789", "26 rue d'henry",magasin);
+		magasin.ajouteClient(client);
+		magasin.ajouteClient(client1);
+		magasin.ajouteClient(client2);
+		magasin.ajouteVendeur(vendeur);
+		magasin.ajouteVendeur(vendeur2);
 		
 		Commande commande = new Commande(LocalDate.now(),vendeur,client);
 		vendeur.ajouteCommande(commande);
 		client.ajouteCommande(commande);
 		LigneCommande ligneCommande = new LigneCommande(2,commande,iphone15);
-        commande.ajouteLigneCommande(ligneCommande);
-		//GestionVente vente = new GestionVente(m);
-		//StatClient statC = new StatClient(m);
+  commande.ajouteLigneCommande(ligneCommande);
+	
 		
 	}
 
